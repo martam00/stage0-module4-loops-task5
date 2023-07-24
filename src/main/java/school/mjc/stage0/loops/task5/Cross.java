73146d457d6a5d2eb57aedd8ce48e49959e18a6e
martam00 <marta.michniacka1@gmail.com>
2023-07-24 17:06:09 +0200

    loops

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,5 +3,23 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public void printCross(int sideLength) {
 
+        //3
+        //  8
+        // 888
+        //  8
+
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (j == sideLength / 2) {
+                    System.out.print("8");
+                } else if (i == sideLength / 2) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
